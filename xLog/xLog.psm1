588a3257121bbeb5fde9fe847938a435d11952cd
Fin392 <63017766+FIN392@users.noreply.github.com>
2020-04-08 10:57:53 +0200

    Add files via upload

--- xLog/xLog.psm1
+++ xLog/xLog.psm1
@@ -1,6 +1,13 @@
-
-
-
+#
+# xLog
+#
+# Author      : FIN392 - fin392@gmail.com
+# Last update : 2020-04-04
+# Version     : 0.1 - Initial release
+# 
+# For complete information on this PowerShell module go to:
+# https://github.com/FIN392/PowerShell/blob/master/xLog/README
+#
 
 
 # Initialize-xLog [-LogID] <string> [-File] <string> [-Format] {TXT | CSV | XML | HTML | JSON} [-UTC]
@@ -10,12 +17,12 @@ function Initialize-xLog {
         Initialize a log file.
 
     .DESCRIPTION
-        A global variable with name 'xLogID_+{Log ID}' is created with information to be used by functions 'Write-xLog' and 'Close-xLog'.
+        A global variable with name 'xLogID_{Log ID}' is created with information to be used by functions 'Write-xLog' and 'Close-xLog'.
         This global variable is an array with 3 values:
-            - Log file name
+            - Log filename
             - Log format
             - UTC value
-        The specified file is created (including the full path if necessary) and first headers lines are written in.
+        The specified file is created (including the full path if necessary) and headers lines are written in.
         VERBOSE Common parameter is available for detailed information of process.
 
     .PARAMETER LogID
@@ -25,23 +32,23 @@ function Initialize-xLog {
 
     .PARAMETER File
         File name where log entries will be written.
-        ATTENTION: If the file already exists will be overwritten without notice.
+        ATTENTION: If the file already exists will be OVERWRITTEN without notice.
         Mandatory.
         Example: 'C:\TEMP\Log.xt', '\\SERVER\SHARE\output.html', 'C:\Apps\Events.json', etc.
 
     .PARAMETER Format
         Log file format.
-        It can be:
+        Accepted values: 
             TXT   To be seen on a console.
             CSV   To be exported to other applications.
             XML   To be managed by XML viewers.
             HTML  To be displayed on a web browser.
-            JSON  To exchange infomation with Java applications.
+            JSON  To exchange information with Java applications.
         Mandatory.
 
     .PARAMETER UTC
-        Establish what will be the time zone and format of the timestamp.
-        If UTC parameter is present, timestamp will be on UTC time zone (aka Zulu time) and format ISO 8601, '{yyyyMMdd}T{HHmmssffff}Z'.
+        Establish the time zone and format for the timestamp.
+        If UTC parameter is present, the timestamp will be on UTC time zone (aka Zulu time) and format ISO 8601, '{yyyyMMdd}T{HHmmssffff}Z'.
         Otherwise timestamp will be on local time zone and format '{yyyy}-{MM}-{dd} {HH}:{mm}:{ss}.{ffff}'. Column header will include the time zone. For example '(UTC+02:00)'.
         Optional.
 
@@ -49,90 +56,25 @@ function Initialize-xLog {
         None.
 
     .OUTPUTS
-        Log file created and global variable defined.
+        None.
 
     .EXAMPLE
         Initialize-xLog -LogID MasterLog -File '.\Logs\20200407.txt' -Format TXT
-        Create a log file named '20200407.txt' on folder '.\Logs'.
-        Add header with this format:
-            Timestamp (UTC+02:00)    Severity Message                                      
-            ------------------------ -------- ---------------------------------------------
-            2020-04-07 22:51:18.4794 DEBUG    This is a debug text                         
-            2020-04-07 22:51:18.4904 INFO     This is a information text                   
-            2020-04-07 22:51:18.5014 WARN     This is a warning text                       
-            2020-04-07 22:51:18.5374 ERROR    This is a information long text with many cha
-                                              racters and many information                 
-            2020-04-07 22:51:18.5604 FATAL    This is an error text                        
-            ------------------------ -------- ---------------------------------------------
+
+        Create a TXT log file named '20200407.txt' on folder '.\Logs'.
+        Timestamp will be set to local time zone and format '{yyyy}-{MM}-{dd} {HH}:{mm}:{ss}.{ffff}'.
 
     .EXAMPLE
         Initialize-xLog -LogID LogProcess01 -File 'C:\Outputs\Process01.csv' -Format CSV -UTC
-        Create a log file named 'Process01.csv' on folder 'C:\Outputs'.
-        Add header with this format:
-            "Timestamp","Severity","Message"
-            "20200405T2356267118Z","DEBUG","This is a debug text"
-            "20200405T2356267338Z","INFO","This is a information text"
-            "20200405T2356267558Z","WARN","This is a warning text"
-            "20200405T2356267788Z","ERROR","This is a information long text with many characters and many information"
-            "20200405T2356268028Z","FATAL","This is an error text"
-            "","","***EOF***"
 
-    .EXAMPLE
-        Initialize-xLog -LogID ERRORS -File '.\ErrorLog.xml' -Format XML -UTC
-        Create a log file named 'ErrorLog.xml' on current folder.
-        Add header with this format:
-            <?xml version="1.0"?>
-            <Log>
-	            <Entry><Timestamp>20200405T2356269568Z</Timestamp><Severity>DEBUG</Severity><Message>This is a debug text</Message></Entry>
-	            <Entry><Timestamp>20200405T2356269808Z</Timestamp><Severity>INFO</Severity><Message>This is a information text</Message></Entry>
-	            <Entry><Timestamp>20200405T2356270048Z</Timestamp><Severity>WARN</Severity><Message>This is a warning text</Message></Entry>
-	            <Entry><Timestamp>20200405T2356270318Z</Timestamp><Severity>ERROR</Severity><Message>This is a information long text with many characters and many information</Message></Entry>
-	            <Entry><Timestamp>20200405T2356270598Z</Timestamp><Severity>FATAL</Severity><Message>This is an error text</Message></Entry>
-            </Log>
-
-    .EXAMPLE
-        Initialize-xLog -LogID L001 -File '\\THOR\APPS\Logs\001.html' -Format HTML
-        Create a log file named '001.html' on folder '\\THOR\APPS\Logs'.
-        Add header with this format:
-            <!DOCTYPE html>
-            <html>
-	            <head>
-		            <style>
-			            table, th, td {border:1px solid black;border-collapse:collapse;}
-			            th, td {padding:5px;text-align:left;}
-		            </style>
-	            </head>
-	            <body>
-		            <table>
-			            <tr><th>Timestamp (UTC+02:00)</th><th>Severity</th><th>Message</th></tr>
-			            <tr><td>2020-04-06 01:56:27.1558</td><td style="background-color:Green;">DEBUG</td><td>This is a debug text</td></tr>
-			            <tr><td>2020-04-06 01:56:27.2028</td><td style="background-color:Green;">INFO</td><td>This is a information text</td></tr>
-			            <tr><td>2020-04-06 01:56:27.2428</td><td style="background-color:Yellow;">WARN</td><td>This is a warning text</td></tr>
-			            <tr><td>2020-04-06 01:56:27.2878</td><td style="background-color:Red;">ERROR</td><td>This is a information long text with many characters and many information</td></tr>
-			            <tr><td>2020-04-06 01:56:27.3168</td><td style="background-color:Red;">FATAL</td><td>This is an error text</td></tr>
-		            </table>
-	            </body>
-            </html>
-
-    .EXAMPLE
-        Initialize-xLog -LogID GeneralLog -File 'X:\EXIT\OUT.json' -Format JSON -UTC
-        Create a log file named 'OUT.json' on folder 'X:\EXIT'.
-        Add header with this format:
-            [
-                {"Timestamp":"20200405T2356276228Z","Severity":"DEBUG","Message":"This is a debug text"},
-                {"Timestamp":"20200405T2356276658Z","Severity":"INFO","Message":"This is a information text"},
-                {"Timestamp":"20200405T2356276888Z","Severity":"WARN","Message":"This is a warning text"},
-                {"Timestamp":"20200405T2356277158Z","Severity":"ERROR","Message":"This is a information long text with many characters and many information"},
-                {"Timestamp":"20200405T2356277338Z","Severity":"FATAL","Message":"This is an error text"},
-            ]
+        Create a CSV log file named 'Process01.csv' on folder 'C:\Outputs'.
+        Timestamp will be set to UTC and ISO 8601 format ('{yyyyMMdd}T{HHmmssffff}Z').
 
     .LINK
-        https://github.com/FIN392/PowerShell
+        https://github.com/FIN392/PowerShell/tree/master/xLog
 
     .NOTES
-        Author: FIN392
-        Last Edit: 2020-04-07
-        Version 1.0 - Iinitial release
+        Author: FIN392 - fin392@gmail.com
 #>
     [CmdletBinding()]
     param (
@@ -222,10 +164,13 @@ Export-ModuleMember -Function Initialize-xLog
 function Write-xLog {
 <# 
     .SYNOPSIS
-        Write a message in a log file.
+        Write a entry in a log file.
 
     .DESCRIPTION
-        A new entry is appended to the log file with following information: timestamp (Local time or UTC based on -UTC parameter during initialation), severety and message.
+        A new entry is appended to the log file with following information:
+            - Timestamp (Local time or UTC based on -UTC parameter during initialation)
+            - Severety
+            - Message.
         VERBOSE Common parameter is available for detailed information of process.
 
     .PARAMETER LogID
@@ -249,11 +194,7 @@ function Write-xLog {
     .PARAMETER Console
         Establish if the entry should be displayed on console.
         Message will be displayed in the following format:
-            2020-04-08 07:27:31.7145 | DEBUG | This is a debug text
-            2020-04-08 07:27:31.7334 | INFO  | This is a information text
-            2020-04-08 07:27:31.7524 | WARN  | This is a warning text
-            2020-04-08 07:27:31.7665 | ERROR | This is a information long text with many characters and many information
-            2020-04-08 07:27:31.8014 | FATAL | This is an error text
+            {yyyy}-{MM}-{dd} {HH}:{mm}:{ss}.{ffff} | {Severity} | {Message}
         Severity is shown in color.
         Optional.
 
@@ -261,89 +202,47 @@ function Write-xLog {
         None.
 
     .OUTPUTS
-        New line added to log file.
+        None.
 
     .EXAMPLE
         Write-xLog -LogID MasterLog -Severity DEBUG -Message 'This is a debug text' -Console
+
         Display this line on console:
             2020-04-07 22:51:18.4794 | DEBUG | This is a debug text
-        Add a line with this format:
-            Timestamp (UTC+02:00)    Severity Message                                      
-            ------------------------ -------- ---------------------------------------------
+        If the log was initialized as TXT, a line with this format will e added:
             2020-04-07 22:51:18.4794 DEBUG    This is a debug text                         
-            2020-04-07 22:51:18.4904 INFO     This is a information text                   
-            2020-04-07 22:51:18.5014 WARN     This is a warning text                       
-            2020-04-07 22:51:18.5374 ERROR    This is a information long text with many cha
-                                              racters and many information                 
-            2020-04-07 22:51:18.5604 FATAL    This is an error text                        
-            ------------------------ -------- ---------------------------------------------
 
     .EXAMPLE
         Write-xLog -LogID LogProcess01 -Severity INFO -Message 'This is a information text'
-        Add a line with this format:
-            "Timestamp","Severity","Message"
-            "20200405T2356267118Z","DEBUG","This is a debug text"
+
+        If the log was initialized as CSV, a line with this format will e added:
             "20200405T2356267338Z","INFO","This is a information text"
-            "20200405T2356267558Z","WARN","This is a warning text"
-            "20200405T2356267788Z","ERROR","This is a information long text with many characters and many information"
-            "20200405T2356268028Z","FATAL","This is an error text"
-            "","","***EOF***"
 
     .EXAMPLE
         Write-xLog -LogID ERRORS -Severity WARN -Message 'This is a warning text' -Console
+
         Display this line on console:
             2020-04-05 23:56:27.0048 | WARN  | This is a warning text
-        Add a line with this format:
-            <?xml version="1.0"?>
-            <Log>
-	            <Entry><Timestamp>20200405T2356269568Z</Timestamp><Severity>DEBUG</Severity><Message>This is a debug text</Message></Entry>
-	            <Entry><Timestamp>20200405T2356269808Z</Timestamp><Severity>INFO</Severity><Message>This is a information text</Message></Entry>
-	            <Entry><Timestamp>20200405T2356270048Z</Timestamp><Severity>WARN</Severity><Message>This is a warning text</Message></Entry>
-	            <Entry><Timestamp>20200405T2356270318Z</Timestamp><Severity>ERROR</Severity><Message>This is a information long text with many characters and many information</Message></Entry>
-	            <Entry><Timestamp>20200405T2356270598Z</Timestamp><Severity>FATAL</Severity><Message>This is an error text</Message></Entry>
-            </Log>
+        If the log was initialized as XML, a line with this format will e added:
+	        <Entry><Timestamp>20200405T2356270048Z</Timestamp><Severity>WARN</Severity><Message>This is a warning text</Message></Entry>
 
     .EXAMPLE
-        Write-xLog -LogID L001 -Severity ERROR -Message 'This is a information long text with many characters and many information'
-        Add a line with this format:
-            <!DOCTYPE html>
-            <html>
-	            <head>
-		            <style>
-			            table, th, td {border:1px solid black;border-collapse:collapse;}
-			            th, td {padding:5px;text-align:left;}
-		            </style>
-	            </head>
-	            <body>
-		            <table>
-			            <tr><th>Timestamp (UTC+02:00)</th><th>Severity</th><th>Message</th></tr>
-			            <tr><td>2020-04-06 01:56:27.1558</td><td style="background-color:Green;">DEBUG</td><td>This is a debug text</td></tr>
-			            <tr><td>2020-04-06 01:56:27.2028</td><td style="background-color:Green;">INFO</td><td>This is a information text</td></tr>
-			            <tr><td>2020-04-06 01:56:27.2428</td><td style="background-color:Yellow;">WARN</td><td>This is a warning text</td></tr>
-			            <tr><td>2020-04-06 01:56:27.2878</td><td style="background-color:Red;">ERROR</td><td>This is a information long text with many characters and many information</td></tr>
-			            <tr><td>2020-04-06 01:56:27.3168</td><td style="background-color:Red;">FATAL</td><td>This is an error text</td></tr>
-		            </table>
-	            </body>
-            </html>
+        Write-xLog -LogID L001 -Severity ERROR -Message 'This is a error text'
+
+        If the log was initialized as HTML, a line with this format will e added:
+		    <tr><td>2020-04-06 01:56:27.2878</td><td style="background-color:Red;">ERROR</td><td>This is a error text</td></tr>
 
     .EXAMPLE
         Write-xLog -LogID GeneralLog -Severity FATAL -Message 'This is an error text'
-        Add a line with this format:
-            [
-                {"Timestamp":"20200405T2356276228Z","Severity":"DEBUG","Message":"This is a debug text"},
-                {"Timestamp":"20200405T2356276658Z","Severity":"INFO","Message":"This is a information text"},
-                {"Timestamp":"20200405T2356276888Z","Severity":"WARN","Message":"This is a warning text"},
-                {"Timestamp":"20200405T2356277158Z","Severity":"ERROR","Message":"This is a information long text with many characters and many information"},
-                {"Timestamp":"20200405T2356277338Z","Severity":"FATAL","Message":"This is an error text"},
-            ]
+
+        If the log was initialized as JSON, a line with this format will e added:
+            {"Timestamp":"20200405T2356277338Z","Severity":"FATAL","Message":"This is an error text"},
 
     .LINK
-        https://github.com/FIN392/PowerShell
+        https://github.com/FIN392/PowerShell/tree/master/xLog
 
     .NOTES
-        Author: FIN392
-        Last Edit: 2020-04-07
-        Version 1.0 - Iinitial release
+        Author: FIN392 - fin392@gmail.com
 #>
     [CmdletBinding()]
     param (
@@ -433,19 +332,18 @@ function Close-xLog {
         None.
 
     .OUTPUTS
-        Footer lines are written in the log file.
+        None.
 
     .EXAMPLE
         Close-xLog -LogID MasterLog
-        Add footer lines to log file.
+
+        Footer lines are added to log file and global variable is removed.
 
     .LINK
-        https://github.com/FIN392/PowerShell
+        https://github.com/FIN392/PowerShell/tree/master/xLog
 
     .NOTES
-        Author: FIN392
-        Last Edit: 2020-04-07
-        Version 1.0 - Iinitial release
+        Author: FIN392 - fin392@gmail.com
 #>
     [CmdletBinding()]
     param (
@@ -488,5 +386,4 @@ function Close-xLog {
 
 }
 Export-ModuleMember -Function Close-xLog
-
 
