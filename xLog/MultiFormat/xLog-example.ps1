a7474bcef67c46d623e87a22fbd495b0ce4bb90b
Fin392 <63017766+FIN392@users.noreply.github.com>
2020-05-04 11:46:25 +0200

    v0

--- xLog/MultiFormat/xLog-example.ps1
+++ xLog/MultiFormat/xLog-example.ps1
@@ -9,31 +9,31 @@ Initialize-xLog -LogID LOG05 -File '.\Logs\xLog.json' -Format JSON -UTC
 Write-xLog -LogID LOG01 -Console -Severity DEBUG -Message 'This is a debug text'
 Write-xLog -LogID LOG01 -Console -Severity INFO  -Message 'This is a information text'
 Write-xLog -LogID LOG01 -Console -Severity WARN  -Message 'This is a warning text'
-Write-xLog -LogID LOG01 -Console -Severity ERROR -Message 'This is a error long text with many characters and many information'
+Write-xLog -LogID LOG01 -Console -Severity ERROR -Message 'LOREM IPSUM. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nisi arcu, commodo et ante id, tempus rutrum diam. Fusce fermentum aliquet metus ut posuere. Etiam at libero augue. Praesent at enim fermentum, porta nulla quis, elementum leo. Nulla fermentum diam a neque posuere ultricies. Praesent sem lorem, aliquam et purus.'
 Write-xLog -LogID LOG01 -Console -Severity FATAL -Message 'This is an fatal text'
 
 Write-xLog -LogID LOG02 -Console -Severity DEBUG -Message 'This is a debug text'
 Write-xLog -LogID LOG02 -Console -Severity INFO  -Message 'This is a information text'
 Write-xLog -LogID LOG02 -Console -Severity WARN  -Message 'This is a warning text'
-Write-xLog -LogID LOG02 -Console -Severity ERROR -Message 'This is a error long text with many characters and many information'
+Write-xLog -LogID LOG02 -Console -Severity ERROR -Message 'LOREM IPSUM. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nisi arcu, commodo et ante id, tempus rutrum diam. Fusce fermentum aliquet metus ut posuere. Etiam at libero augue. Praesent at enim fermentum, porta nulla quis, elementum leo. Nulla fermentum diam a neque posuere ultricies. Praesent sem lorem, aliquam et purus.'
 Write-xLog -LogID LOG02 -Console -Severity FATAL -Message 'This is an fatal text'
 
 Write-xLog -LogID LOG03 -Console -Severity DEBUG -Message 'This is a debug text'
 Write-xLog -LogID LOG03 -Console -Severity INFO  -Message 'This is a information text'
 Write-xLog -LogID LOG03 -Console -Severity WARN  -Message 'This is a warning text'
-Write-xLog -LogID LOG03 -Console -Severity ERROR -Message 'This is a error long text with many characters and many information'
+Write-xLog -LogID LOG03 -Console -Severity ERROR -Message 'LOREM IPSUM. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nisi arcu, commodo et ante id, tempus rutrum diam. Fusce fermentum aliquet metus ut posuere. Etiam at libero augue. Praesent at enim fermentum, porta nulla quis, elementum leo. Nulla fermentum diam a neque posuere ultricies. Praesent sem lorem, aliquam et purus.'
 Write-xLog -LogID LOG03 -Console -Severity FATAL -Message 'This is an fatal text'
 
 Write-xLog -LogID LOG04 -Console -Severity DEBUG -Message 'This is a debug text'
 Write-xLog -LogID LOG04 -Console -Severity INFO  -Message 'This is a information text'
 Write-xLog -LogID LOG04 -Console -Severity WARN  -Message 'This is a warning text'
-Write-xLog -LogID LOG04 -Console -Severity ERROR -Message 'This is a error long text with many characters and many information'
+Write-xLog -LogID LOG04 -Console -Severity ERROR -Message 'LOREM IPSUM. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nisi arcu, commodo et ante id, tempus rutrum diam. Fusce fermentum aliquet metus ut posuere. Etiam at libero augue. Praesent at enim fermentum, porta nulla quis, elementum leo. Nulla fermentum diam a neque posuere ultricies. Praesent sem lorem, aliquam et purus.'
 Write-xLog -LogID LOG04 -Console -Severity FATAL -Message 'This is an fatal text'
 
 Write-xLog -LogID LOG05 -Console -Severity DEBUG -Message 'This is a debug text'
 Write-xLog -LogID LOG05 -Console -Severity INFO  -Message 'This is a information text'
 Write-xLog -LogID LOG05 -Console -Severity WARN  -Message 'This is a warning text'
-Write-xLog -LogID LOG05 -Console -Severity ERROR -Message 'This is a error long text with many characters and many information'
+Write-xLog -LogID LOG05 -Console -Severity ERROR -Message 'LOREM IPSUM. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nisi arcu, commodo et ante id, tempus rutrum diam. Fusce fermentum aliquet metus ut posuere. Etiam at libero augue. Praesent at enim fermentum, porta nulla quis, elementum leo. Nulla fermentum diam a neque posuere ultricies. Praesent sem lorem, aliquam et purus.'
 Write-xLog -LogID LOG05 -Console -Severity FATAL -Message 'This is an fatal text'
 
 Close-xLog -LogID LOG01
