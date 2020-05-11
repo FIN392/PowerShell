5baedd9f4be4c3f75f709aa4adbfd7b56ac93a6f
Fin392 <63017766+FIN392@users.noreply.github.com>
2020-05-11 09:30:48 +0200

    v0

--- xLog/xLog-example.ps1
+++ xLog/xLog-example.ps1
@@ -2,7 +2,7 @@
 Import-Module -Name '.\xLog.psm1' -Force
 
 # Then the log should be initialized. At this stage you choose several format options
-$MyLog = Initialize-xLog -File '.\Logs\xLog.txt' -LocalTime -Reverse -Console -Encoding 'Unicode'
+$MyLog = Initialize-xLog -File '.\xLogs\xLog_LocalTime_Reverse_Unicode.txt' -LocalTime -Reverse -Console -Encoding 'Unicode'
 
 # Now, simply add messages with the chosen severity to the log
 Write-xLog -Log $MyLog -Severity DEBUG -Message ( 'PowerShell version ' + ( $PSVersionTable.PSVersion ) )
@@ -14,30 +14,26 @@ Write-xLog -Log $MyLog -Severity INFO  -Message 'LOREM IPSUM
 	Nulla fermentum diam a neque posuere ultricies.
 	Praesent sem lorem, aliquam et purus.'
 Write-xLog -Log $MyLog -Severity WARN  -Message ( 'Current path: ' + ( Get-Location ).Path )
-Write-xLog -Log $MyLog -Severity ERROR -Message ( (Get-Host).CurrentCulture.Name )
-Write-xLog -Log $MyLog -Severity FATAL -Message ( (Get-Host).CurrentCulture.DisplayName )
+Write-xLog -Log $MyLog -Severity ERROR -Message ( (Get-Host).CurrentCulture.Name + ' | ' + (Get-Host).CurrentCulture.DisplayName )
+Write-xLog -Log $MyLog -Severity FATAL -Message ( 'Spanish special characters: áéíóúüñÁÉÍÓÚÜÑ' )
 
+# Just another example. Different initialization...
+$MyLog = Initialize-xLog -File '.\xLogs\xLog_UTC_ASCII.txt' -Encoding 'ASCII'
 
+# ...same entries.
+Write-xLog -Log $MyLog -Severity DEBUG -Message ( 'PowerShell version ' + ( $PSVersionTable.PSVersion ) )
+Write-xLog -Log $MyLog -Severity INFO  -Message 'LOREM IPSUM
+	Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+	Phasellus nisi arcu, commodo et ante id, tempus rutrum diam.
+	Fusce fermentum aliquet metus ut posuere. Etiam at libero augue.
+	Praesent at enim fermentum, porta nulla quis, elementum leo.
+	Nulla fermentum diam a neque posuere ultricies.
+	Praesent sem lorem, aliquam et purus.'
+Write-xLog -Log $MyLog -Severity WARN  -Message ( 'Current path: ' + ( Get-Location ).Path )
+Write-xLog -Log $MyLog -Severity ERROR -Message ( (Get-Host).CurrentCulture.Name + ' | ' + (Get-Host).CurrentCulture.DisplayName )
+Write-xLog -Log $MyLog -Severity FATAL -Message ( 'Spanish special characters: áéíóúüñÁÉÍÓÚÜÑ' )
 
 # Finally, if there are not more scripts running in the session, the module can be removed
 Remove-Module -Name xLog -Force
 
-# How looks like the created log:
-# 	2020-05-11 08:52:38.615 (+02:00) | FATAL | Spanish (Spain, International Sort)
-# 	2020-05-11 08:52:38.513 (+02:00) | ERROR | es-ES
-# 	2020-05-11 08:52:38.488 (+02:00) | WARN  | Current path: C:\Users\FERNAJL\OneDrive - Abbott\GitHub\PowerShell\xLog\Straight
-# 	2020-05-11 08:52:38.461 (+02:00) | INFO  | LOREM IPSUM
-# 	        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-# 	        Phasellus nisi arcu, commodo et ante id, tempus rutrum diam.
-# 	        Fusce fermentum aliquet metus ut posuere. Etiam at libero augue.
-# 	        Praesent at enim fermentum, porta nulla quis, elementum leo.
-# 	        Nulla fermentum diam a neque posuere ultricies.
-# 	        Praesent sem lorem, aliquam et purus.
-# 	2020-05-11 08:52:38.425 (+02:00) | DEBUG | PowerShell version 7.0.0
-
-
-# Import-Module -Name '.\xLog.psm1' -Force
-# Get-Help Initialize-xLog -Detailed
-# Get-Help Write-xLog -Detailed
-# Remove-Module -Name xLog -Force
-
+# Check files '.\xLogs\xLog_LocalTime_Reverse_Unicode.txt' and '.\xLogs\xLog_UTC_ASCII.txt'.
