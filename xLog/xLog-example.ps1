55b22779805c39d9897c060405218908455c00a8
Fin392 <fin392@gmail.com>
2021-01-10 22:07:56 +0100

    Update xLog-example.ps1

--- xLog/xLog-example.ps1
+++ xLog/xLog-example.ps1
@@ -18,20 +18,20 @@ Write-xLog -Log $MyLog -Severity ERROR -Message ( (Get-Host).CurrentCulture.Name
 Write-xLog -Log $MyLog -Severity FATAL -Message ( 'Spanish special characters: áéíóúüñÁÉÍÓÚÜÑ' )
 
 # Just another example. Different initialization but...
-$MyLog = Initialize-xLog -File '.\xLogs\xLog_UTC_ASCII.txt' -Encoding 'ASCII'
+$MyLog2 = Initialize-xLog -File '.\xLogs\xLog_UTC_ASCII.txt' -Encoding 'ASCII'
 
 # ...same entries. (See different encoding for Spanish special characters)
-Write-xLog -Log $MyLog -Severity DEBUG -Message ( 'PowerShell version ' + ( $PSVersionTable.PSVersion ) )
-Write-xLog -Log $MyLog -Severity INFO  -Message 'LOREM IPSUM
+Write-xLog -Log $MyLog2 -Severity DEBUG -Message ( 'PowerShell version ' + ( $PSVersionTable.PSVersion ) )
+Write-xLog -Log $MyLog2 -Severity INFO  -Message 'LOREM IPSUM
 	Lorem ipsum dolor sit amet, consectetur adipiscing elit.
 	Phasellus nisi arcu, commodo et ante id, tempus rutrum diam.
 	Fusce fermentum aliquet metus ut posuere. Etiam at libero augue.
 	Praesent at enim fermentum, porta nulla quis, elementum leo.
 	Nulla fermentum diam a neque posuere ultricies.
 	Praesent sem lorem, aliquam et purus.'
-Write-xLog -Log $MyLog -Severity WARN  -Message ( 'Current path: ' + ( Get-Location ).Path )
-Write-xLog -Log $MyLog -Severity ERROR -Message ( (Get-Host).CurrentCulture.Name + ' | ' + (Get-Host).CurrentCulture.DisplayName )
-Write-xLog -Log $MyLog -Severity FATAL -Message ( 'Spanish special characters: áéíóúüñÁÉÍÓÚÜÑ' )
+Write-xLog -Log $MyLog2 -Severity WARN  -Message ( 'Current path: ' + ( Get-Location ).Path )
+Write-xLog -Log $MyLog2 -Severity ERROR -Message ( (Get-Host).CurrentCulture.Name + ' | ' + (Get-Host).CurrentCulture.DisplayName )
+Write-xLog -Log $MyLog2 -Severity FATAL -Message ( 'Spanish special characters: áéíóúüñÁÉÍÓÚÜÑ' )
 
 # Finally, if there are not more scripts running in the session, the module can be removed
 Remove-Module -Name xLog -Force
