5f1b8c1b0d7205a8098345b86014eadb00febe2a
Fin392 <fin392@gmail.com>
2024-04-19 22:11:29 +0200

    Update PowerShell code embedded in a BAT script.cmd

--- How to/PowerShell code embedded in a BAT script.cmd
+++ How to/PowerShell code embedded in a BAT script.cmd
@@ -1,4 +1,7 @@
 # 2>NUL & @CLS & PUSHD "%~dp0" & "%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoLogo -NoProfile -ExecutionPolicy Bypass "[IO.File]::ReadAllText('%~f0') | Invoke-Expression" & POPD & EXIT /B
 
-write-host "qwe"
-start-sleep 5
+#
+# You Pwershell code goes here
+#
+Write-Host "Hello, World!"
+Start-Sleep 5
