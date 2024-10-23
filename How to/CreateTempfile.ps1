f89114f201a2b0d4e81515783bf822fb3ec13587
Fin392 <fin392@gmail.com>
2024-10-23 20:34:12 +0200

    Create CreateTempfile.ps1

---
+++ How to/CreateTempfile.ps1
@@ -0,0 +1,5 @@
+$tempFileName = [System.IO.Path]::GetTempFileName()
+
+# The file is created. Remember to delete it once you are done using it.
+# Example of created file: C:\Users\fin392\AppData\Local\Temp\tmp135A.tmp
+
