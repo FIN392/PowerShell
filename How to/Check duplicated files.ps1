40a8013948abaac522e0918f116a94a2eaf5a48f
Fin392 <fin392@gmail.com>
2025-08-10 07:19:33 +0200

    Update Check duplicated files.ps1

--- How to/Check duplicated files.ps1
+++ How to/Check duplicated files.ps1
@@ -1,5 +1,26 @@
-'.' | Get-ChildItem -Recurse |
-    Get-FileHash -ErrorAction SilentlyContinue |
-        Group-Object 'Hash' |
+#
+#
+#
+
+Measure-Command {
+    $v0 = '.' | Get-ChildItem -File -Recurse |
+        Get-FileHash -ErrorAction SilentlyContinue |
+            Group-Object 'Hash' |
+                Where-Object { $_.Count -gt 1 } |
+                    Select-Object -Property Group -ExpandProperty Group
+}
+
+#
+# 
+#
+Measure-Command {
+    $v1 = '.' | Get-ChildItem -File -Recurse |
+        Group-Object 'Length' |
             Where-Object { $_.Count -gt 1 } |
-                Select-Object -Property Group -ExpandProperty Group
+                Select-Object -Property Group -ExpandProperty Group |
+                    Get-FileHash -ErrorAction SilentlyContinue |
+                        Group-Object 'Hash' |
+                            Where-Object { $_.Count -gt 1 } |
+                                Select-Object -Property Group -ExpandProperty Group
+}                
+
